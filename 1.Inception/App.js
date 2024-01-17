6c6d8e70579294b26b2503ef8cda29e548238363
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-01-17 10:54:28 +0500

    conversion to App.jsx

--- 1.Inception/App.js
+++ 1.Inception/App.js
@@ -1,3 +1,10 @@
-const heading = React.createElement("h1", {}, "Hello World From React CDN");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React CDN"
+);
+
+console.log(heading); // this is a react element. A normal javascript object.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
