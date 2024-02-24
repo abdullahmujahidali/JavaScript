66e39d8caeb927d3f91bb005f8a65f70f7405ed0
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-02-24 10:14:16 +0500

    React component

--- 3.LayingTheFoundation/App.js
+++ 3.LayingTheFoundation/App.js
@@ -7,14 +7,20 @@ const heading = React.createElement(
   "Laying the foundation"
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
 // JSX (transpiled before it reaches the JS ) - PARCEL - BABEL
-const jsxHeading = (
+const Title = () => (
   <h1 id="heading" className="head">
-    {" "}
     Laying the foundation using JSX
   </h1>
 );
 
-root.render(jsxHeading);
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">React Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
