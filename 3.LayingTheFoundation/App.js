ed27ceea308e06cea84b9ac2666509c1b132454e
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-02-24 09:12:10 +0500

    created app from scratch

--- 3.LayingTheFoundation/App.js
+++ 3.LayingTheFoundation/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1" }, "This is first h1 tag"),
-    React.createElement("h2", { id: "h2" }, "This is a second h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "h1" }, "I am a nested h1 tag"),
-    React.createElement("h2", { id: "h2" }, "I am a sibling h2 tag"),
-  ]),
-]);
-
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World From React CDN"
+  "Laying the foundation"
 );
 
-console.log(heading); // this is a react element. A normal javascript object.
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
